7c75448ab560add416a373b4ff90337f59725db9
StelioBrooky <brookystel@myvuw.ac.nz>
2022-10-04 16:40:05 +1300

    Added functioning music

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 
+export interface Track {
+  name: string;
+  path: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+  playlists: Track[] = [{
+    name: 'Coconut',
+    path: 'assets/mp3/Coconut.mp3'
+  },
+]
+
+activeTrack: Track = null;
 
   constructor() {}
 
+  start(track: Track) {
+    this.activeTrack = track;
+    const audio = new Audio(track.path);
+    audio.play();
+  }
 }
