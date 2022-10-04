0932512fd2a718ad5b0af6edbaafdbc7cbaf457e
StelioBrooky <brookystel@myvuw.ac.nz>
2022-10-05 12:13:21 +1300

    funcioning play/pause button added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Howl } from 'howler';
+
+
 
 export interface Track {
   name: string;
@@ -18,13 +21,43 @@ export class HomePage {
   },
 ]
 
+howler: Howl = null;
 activeTrack: Track = null;
+playing: boolean = false;
 
   constructor() {}
 
   start(track: Track) {
-    this.activeTrack = track;
-    const audio = new Audio(track.path);
-    audio.play();
+    if (this.howler) {
+      this.howler.stop();
+    }
+
+    this.howler = new Howl({
+      src: [track.path],
+      html5: true,
+      onplay: () => {
+        this.playing = true;
+        console.log('onplay');
+        this.activeTrack = track;
+      },
+      onend: () => {
+        console.log('onend');
+      }
+    });
+    this.howler.play();
+}
+
+playPause(status) {
+    //if it is playing, pause it, if it is paused, play it
+    this.playing = !status;
+    if (status) {
+      this.howler.pause();
+    }
+    else {
+      this.howler.play();
+    }
+
+
   }
+
 }
