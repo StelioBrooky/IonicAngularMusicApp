0f1361119413a323507d1b441cca1815e9d99418
StelioBrooky <brookystel@myvuw.ac.nz>
2022-10-06 12:58:01 +1300

    Functioning authentication

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -8,13 +10,44 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private route: Router) { }
+  currentUser = {
+    email: '',
+    password: ''
+  };
+
+  constructor(private route: Router, public ngFireAuth: AngularFireAuth) {
+    
+
+   }
 
   ngOnInit() {
   }
 
-  homeNav(){
-    this.route.navigate(['/home']);
+  async homeNav(){
+    const user = await this.ngFireAuth.signInWithEmailAndPassword(this.currentUser.email, this.currentUser.password);
+    if(user.user.email){
+      this.route.navigate(['home']);
+    }
+    else{
+      alert("Invalid Credentials");
+    }
   }
+
+
+  async signUpNav(){
+    const user = this.ngFireAuth.createUserWithEmailAndPassword(this.currentUser.email, this.currentUser.password);
+
+    if((await user).user.email){
+      alert("Account Created");
+      this.route.navigate(['home']);
+    }
+    else{
+      alert("Invalid Credentials");
+    }
+
+    console.log(user);
+    
+  }
+
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyASKoofNqK3t84WWnR28aMdOE-Y5xhYOPs",
+    authDomain: "musicplayer-1f548.firebaseapp.com",
+    projectId: "musicplayer-1f548",
+    storageBucket: "musicplayer-1f548.appspot.com",
+    messagingSenderId: "419393294204",
+    appId: "1:419393294204:web:f166050e24927832561ef9",
+    measurementId: "G-GR4MXN3MYJ"}
 };
 
 /*
