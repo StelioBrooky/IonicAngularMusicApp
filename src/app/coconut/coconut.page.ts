dea09aea978b6932c40340bddf6be11115602043
StelioBrooky <brookystel@myvuw.ac.nz>
2022-10-06 21:24:29 +1300

    functioning coconut page

--- src/app/coconut/coconut.page.ts
+++ src/app/coconut/coconut.page.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Howl } from 'howler';
+
+export interface Track {
+  name: string;
+  path: string;
+  nav: string;
+}
 
 @Component({
   selector: 'app-coconut',
@@ -7,9 +15,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoconutPage implements OnInit {
 
-  constructor() { }
+  song: Track = {
+    name: 'Coconut',
+    path: 'assets/mp3/Coconut.mp3',
+    nav: 'coconut'
+  }
+
+  howler: Howl = null;
+  activeTrack: Track = null;
+  playing: boolean = false;
+
+  constructor(private route: Router ) { 
+  }
 
   ngOnInit() {
+  }
+
+  start() {
+    if (this.howler) {
+      this.howler.stop();
+    }
+
+    this.howler = new Howl({
+      src: [this.song.path],
+      html5: true,
+      onplay: () => {
+        this.playing = true;
+        console.log('onplay');
+        this.activeTrack = this.song;
+      },
+      onend: () => {
+        console.log('onend');
+      }
+    });
+    this.howler.play();
+}
+
+playPause(status) {
+
+  // if this is our first time playing a track, run the start function
+  if (!this.activeTrack) {
+    this.start();
+  }
+    //if it is playing, pause it, if it is paused, play it
+    this.playing = !status;
+    if (status) {
+      this.howler.pause();
+    }
+    else {
+      this.howler.play();
+    }
+
+  }
+
+  homeNav(){
+    this.route.navigate(['/home']);
   }
 
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -47,46 +47,12 @@ export class HomePage {
     path: 'assets/mp3/Coconut.mp3',
     nav: 'moving'
   },
-]
+];
 
-howler: Howl = null;
-activeTrack: Track = null;
-playing: boolean = false;
+
 
   constructor(private route: Router, ){}
   
-
-  start(track: Track) {
-    if (this.howler) {
-      this.howler.stop();
-    }
-
-    this.howler = new Howl({
-      src: [track.path],
-      html5: true,
-      onplay: () => {
-        this.playing = true;
-        console.log('onplay');
-        this.activeTrack = track;
-      },
-      onend: () => {
-        console.log('onend');
-      }
-    });
-    this.howler.play();
-}
-
-playPause(status) {
-    //if it is playing, pause it, if it is paused, play it
-    this.playing = !status;
-    if (status) {
-      this.howler.pause();
-    }
-    else {
-      this.howler.play();
-    }
-
-  }
 
   loginNav(){
     this.route.navigate(['/login']);
