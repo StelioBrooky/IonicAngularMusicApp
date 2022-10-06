849e39d7f08f47ed42128ba108543ad785fbb886
puagevi <puagevi@ecs.vuw.ac.nz>
2022-10-07 00:41:00 +1300

    Set up code to upload tracks to storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,14 @@ import { AppRoutingModule } from './app-routing.module';
 
 import {AngularFireModule} from '@angular/fire/compat';
 import {AngularFireAuthModule} from '@angular/fire/compat/auth';
+//import { AngularFireDatabaseModule } from '@angular/fire/compat/database'; // Test after making a database. Add AngularFireDatabaseModule to imports too.
+import { AngularFireStorageModule } from '@angular/fire/compat/storage'; // Test after setting up storage.
 import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,  AngularFireModule.initializeApp(environment.firebase), AngularFireAuthModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,  AngularFireModule.initializeApp(environment.firebase), AngularFireAuthModule, AngularFireStorageModule],
   providers: [BluetoothSerial, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Howl } from 'howler';
 
-
+//import { AngularFireDatabase } from '@angular/fire/compat/database'; // Test after making database. Add public aFDB: AngularFireDatabase, to constructor too
+import { AngularFireStorage } from '@angular/fire/compat/storage'; // Test after setting up storage.
 
 export interface Track {
   name: string;
@@ -51,7 +52,10 @@ export class HomePage {
 
 
 
-  constructor(private route: Router, ){}
+  constructor(private route: Router, public aFS: AngularFireStorage){
+    //this.addToDB();
+    this.addToStorage();
+  }
   
 
   loginNav(){
@@ -62,5 +66,24 @@ export class HomePage {
   }
   songNav(songNav){
     this.route.navigate(['/'+songNav]);
+  }
+
+  /*addToDB(){
+    // database
+    for (let i of this.playlists) {
+      this.aFDB.object('music/' + i.name).set({
+        songname: i.name,
+        songpath: i.nav,
+      });
+    }
+  }*/
+
+  addToStorage(){ // Upload tracks to storage
+    // storage
+    for (let i of this.playlists) {
+      //this.aFS.ref('music/' + i.name).put();
+      //this.aFS.upload('music/' + i.name, { type: 'audio/mp3' });
+      this.aFS.upload('music/' + i.name, i.path);
+    }
   }
 }
