b34b8c184f19f57c7365af1c5a3eb228db07c6b7
matthesamu1@myvuw.ac.nz <matthesamu1@myvuw.ac.nz>
2022-10-06 14:54:13 +1300

    Adding a seperate screen for bluetooth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,11 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },  {
+    path: 'bluetooth',
+    loadChildren: () => import('./bluetooth/bluetooth.module').then( m => m.BluetoothPageModule)
   },
+
 ];
 
 @NgModule({
